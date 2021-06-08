9b4fbf001fc61458fc8083ae9e09df107a1d1feb
pixarbasim <pixar.basim@gmail.com>
2021-06-08 16:03:04 +0530

    Fixed error handling on page navigation errors

--- index.js
+++ index.js
@@ -98,29 +98,21 @@ module.exports = function (config) {
   };
 
 const gotoPageWithRetries = async function (page, url, retryCount = 10) {
-  if (retryCount > 0) {
+  if (retryCount <= 0) {
+    throw new Error('No retries left!');
+  }
 
-    const onRetry = async (count) => {
-      log('Going to retry');
-      await sleep(250); //Sleep for 250ms
-      return await gotoPageWithRetries(page, url, count);
-    }
+  const onRetry = async (count) => {
+    log('Going to retry');
+    await sleep(250); //Sleep for 250ms
+    return await gotoPageWithRetries(page, url, count);
+  }
 
-    try {
-      const result = await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 }).catch(e => e);
-      if (result instanceof Error) {
-        //Retry in case an error is thrown on the puppeteer navigation
-        log('Failed to load page with error: ' + result.message);
-        if(retryCount > 1){
-          return onRetry(result, retryCount - 1);
-        }
-      }        
-    } catch(err) {
-      log('Failed to load page with error: ' + err.message);
-      return onRetry(retryCount -1);
-    }
-
-    return result;
+  try {
+    await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 });
+  } catch(err) {
+    log('Failed to load page with error: ' + err.message);
+    return onRetry(retryCount -1);
   }
 };
 
